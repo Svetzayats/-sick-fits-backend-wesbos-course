69064e0a72238f9769a5eeeaaeea4de78b6a6fa5
Svetzayats <svetzayats@gmail.com>
2020-04-19 21:48:44 +0500

    Mutation removeFromCart

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -252,6 +252,27 @@ const Mutations = {
                 }
             }
         }, info)
+    },
+
+    async removeFromCart(parent, args, ctx, info) {
+        // find the cart item 
+        const cartItem = await ctx.db.query.cartItem({
+            where: {
+                id: args.id
+            }
+        }, `{ id, user { id }}`);
+        // make sure we found cart item 
+        if (!cartItem) throw new Error('No CartItem found!');
+        // make sure they own that cart item 
+        if (cartItem.user.id !== ctx.request.userId) {
+            throw new Error('Cheatin hahahha');
+        }
+        // delete that cart item 
+        return ctx.db.mutation.deleteCartItem({
+            where: {
+                id: args.id
+            }
+        }, info);
     }
 
 };
